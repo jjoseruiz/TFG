e0ef1d57d5abffa99539b333aa306a8614386384
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-23 21:45:29 +0200

    comité expertos

--- clasificacion.R
+++ clasificacion.R
@@ -5,7 +5,7 @@ library(rpart)
 library(rpart.plot)
 library(dplyr)
 library(randomForest)
-dataset<-read.csv("almacen1000")
+dataset<-read.csv("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/almacen1000")
 dataset<-dataset[,2:ncol(dataset)]
 head(dataset)
 #lesion=filter(dataset,LESION==1)
@@ -19,15 +19,15 @@ prueba=dataset[particion>=0.8,]
 RFmodel = randomForest(LESION~.,data=entrenamiento,na.action = na.omit,ntree=300)
 RFmodel$confusion
 predi_RF=predict(RFmodel,prueba)
-mc_rf=table(predi_Rf,prueba$LESION)
-exac=sum(diag(mc_rf))/sum(mc_rf)
-exac
+mc_rf=table(predi_RF,prueba$LESION)
+exac_rf=sum(diag(mc_rf))/sum(mc_rf)
+exac_rf
 varImpPlot(RFmodel)
 
 
 #para escribir el modelo
 saveRDS(RFmodel,"RandomForest_almacen2000.rds")
-
+RFmodel=readRDS("RandomForest_almacen2000.rds")
 
 library(caret)
 trainData=entrenamiento
@@ -42,7 +42,7 @@ mc_nb=table(prediBayes ,prueba$LESION)
 exac_nb=sum(diag(mc_nb))/sum(mc_nb)
 exac_nb
 saveRDS(bayesiano,"Bayesian_almacen2000.rds")
-
+bayesiano=readRDS("Bayesian_almacen2000.rds")
 
 #bayesiano<-readRD("modeloBayesiano.rds")
 #k-nearest-neighbors
@@ -55,3 +55,20 @@ exac_knn=sum(diag(mc_knn))/sum(mc_knn)
 exac_knn
 
 saveRDS(knn,"Knn_almacen2000.rds")
+
+
+knn=readRDS("Knn_almacen2000.rds")
+
+#Comité de expertos
+resultado=c(1:length(prueba$LESION))
+for(i in 1:length(prueba$LESION))
+{
+  resultado[i]=mfv(c(prediKnn[i],prediBayes[i],predi_RF[[i]])-1)
+}
+
+resultado=factor(resultado)
+resultado
+mc_expertos=table(resultado,prueba$LESION)
+mc_expertos
+exac_expert=sum(diag(mc_expertos))/sum(mc_expertos)
+exac_expert
