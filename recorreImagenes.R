fe3fd57fb684825ee5f19e74abd8d4efcd0a1697
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-30 16:15:16 +0200

    actualización para sacar los valores de las imagenes dado n vector de coordenadas

--- recorreImagenes.R
+++ recorreImagenes.R
@@ -1,13 +1,24 @@
+source("valoresImagen.R")
+source("sacoValorVecinos.R")
 recorreImagenes<-function(IMAGENES,COORDENADAS)
 {
   
   #las IMAGENES VENDRAN EN UNA LISTA 
   #Las coordenadas serán una matriz de coordenadas
-  mat = matrix(nrow = nrow(COORDENADAS),ncol = length(IMAGENES)*27)
+  bol=is.matrix(COORDENADAS)
+  if(bol){
+    mat = matrix(nrow = nrow(COORDENADAS),ncol = length(IMAGENES)*27)
+  }else{
+    mat = matrix(nrow = length(COORDENADAS),ncol = length(IMAGENES)*27)
+  }
   l=0
   for(i in 1:length(IMAGENES)){
     v = valoresImagen(IMAGENES[[i]],COORDENADAS)
-    valores = sacoValorVecinos(IMAGENES[[i]],v)
+    if(bol){
+      valores = sacoValorVecinos(IMAGENES[[i]],v)
+    }else{
+      valores=v
+    }
     mat[1:nrow(valores),(1+l):(l+ncol(valores))]=valores
     l=l+ncol(valores)
   }
