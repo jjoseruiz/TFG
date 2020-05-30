214835b4c300447e9c792a46f2e42abc3dba7caa
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-30 16:14:51 +0200

    actualización para sacar los valores dado un vector

--- valoresImagen.R
+++ valoresImagen.R
@@ -1,22 +1,35 @@
 valoresImagen<-function(IMAGEN,COORDENADAS){
   #27 vecinos por 3 coordenadas cada uno.
-  coord = matrix(nrow = nrow(COORDENADAS),ncol = 27*3,byrow = TRUE)
   if(!is.antsImage(IMAGEN)){
     imagenants = as.antsImage(IMAGEN)
   }else{
     imagenants = IMAGEN
   }
-  for(i in 1:nrow(COORDENADAS)){
-    ventana = getNeighborhoodAtVoxel(imagenants,center = c(COORDENADAS[i,1],COORDENADAS[i,2],COORDENADAS[i,3]),c(1,1,1))
-    coordi = ventana$indices
-    ordenada = c()
-    for(j in 1:nrow(coordi)){
-      ordenada=c(coordi[j,1:3],ordenada)
+  if(is.matrix(COORDENADAS)){
+    coord = matrix(nrow = nrow(COORDENADAS),ncol = 27*3,byrow = TRUE)
+    
+    for(i in 1:nrow(COORDENADAS)){
+      ventana = getNeighborhoodAtVoxel(imagenants,center = c(COORDENADAS[i,1],COORDENADAS[i,2],COORDENADAS[i,3]),c(1,1,1))
+      coordi = ventana$indices
+      ordenada = c()
+      for(j in 1:nrow(coordi)){
+        ordenada=c(coordi[j,1:3],ordenada)
+      }
+      #cada tres columnas tenemos las coordenadas de un vecino
+      coord[i,1:81]=ordenada
     }
-    #cada tres columnas tenemos las coordenadas de un vecino
-    coord[i,1:81]=ordenada
+    #hay que tener en cuenta que los primeros numvoxel/2 son LESION
+    #Los numVoxel/2 siguientes son SANOS
+    return(coord)
+  }else{
+    #DEVUELVE DIRECTAMENTE EL VALOR DE LOS VECINOS
+    coord = matrix(nrow = length(COORDENADAS),ncol = 27)
+    for(i in 1:length(COORDENADAS)){
+      ventana = getNeighborhoodAtVoxel(imagenants,center = obtenCoord(COORDENADAS[i],imagenants),c(1,1,1))
+      coordi = ventana$values
+      coord[i,1:27]=coordi
+      print(paste0("voxel nº = ",i))
+    }
+    return(coord)
   }
-  #hay que tener en cuenta que los primeros numvoxel/2 son LESION
-  #Los numVoxel/2 siguientes son SANOS
-  return(coord)
 }
