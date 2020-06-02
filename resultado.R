6fe736eca313f0f786a056e71b12866dda6b6338
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-06-02 04:07:45 +0200

    script que representará el resultado de la predicción

---
+++ resultado.R
@@ -0,0 +1,13 @@
+source("obtenCoord.R")
+resultado<-function(IMAGEN,COORDENADAS,PREDICCION){
+  cordenadas=c(COORDENADAS)
+  prediccion=c(PREDICCION)
+  valoresLesion=which(cordenadas&prediccion==TRUE)
+  lesiones=array(data=rep(0,192*512*512),dim=c(192,512,512))
+  for(i in 1:length(valoresLesion)){
+    cor=obtenCoord(valoresLesion[i],IMAGEN)
+    lesiones[cor[1],cor[2],cor[3]]=1
+  }
+  copi=maskImage(FLAIR,arr)
+  ortho2(FLAIR,copi,col.y = "red")
+}
