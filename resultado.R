4f15cdf2bf39faa088ad9cd3ec1e16fcc8ef5e87
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-06-04 03:25:04 +0200

    arreglo obtención de resultados

--- resultado.R
+++ resultado.R
@@ -5,9 +5,9 @@ resultado<-function(IMAGEN,COORDENADAS,PREDICCION){
   valoresLesion=which(cordenadas&prediccion==TRUE)
   lesiones=array(data=rep(0,192*512*512),dim=c(192,512,512))
   for(i in 1:length(valoresLesion)){
-    cor=obtenCoord(valoresLesion[i],IMAGEN)
+    cor=obtenCoord(cordenadas[valoresLesion[[i]]],IMAGEN)
     lesiones[cor[1],cor[2],cor[3]]=1
   }
   copi=maskImage(FLAIR,lesiones)
-  ortho2(FLAIR,copi,col.y = "red")
+  ortho2(FLAIR,copi,col.y = "red",useRaster = TRUE)
 }
