4d7a44cb8f9f821c848953f8467c96af2c06cb18
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-06-10 13:47:03 +0200

    PENDIENTE A CAMBIO. USO IQR eliminado fondo con máscara o >0?

--- eligeVoxelPaciente.R
+++ eligeVoxelPaciente.R
@@ -1,6 +1,10 @@
 eligeVoxelPaciente<-function(FLAIR){
-  valores = getPixels(FLAIR,1:dim(FLAIR)[1],1:dim(FLAIR)[2],1:dim(FLAIR)[3])
-  umbral=quantile(valores,0.9999)
-  cordis=which(valores>umbral)
+  #Eliminamos los valores fondo
+  valores = FLAIR[FLAIR>0]
+  #calculamos el umbral a partir del cual es probable que los vóxeles sean lesion
+  umbral = quantile(valores,0.15)+3*IQR(valores)
+  #umbral = quantile(valores,0.999)
+  #calculamos las coordenadas de esos vóxeles
+  cordis = which(FLAIR>=umbral)
   return(cordis)
 }
