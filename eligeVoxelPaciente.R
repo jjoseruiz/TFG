6eee1a825074393aafb8f4c6ba9a09ec625465b6
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-06-11 20:50:14 +0200

    1.5*IQR

--- eligeVoxelPaciente.R
+++ eligeVoxelPaciente.R
@@ -2,7 +2,7 @@ eligeVoxelPaciente<-function(FLAIR){
   #Eliminamos los valores fondo
   valores = FLAIR[FLAIR>0]
   #calculamos el umbral a partir del cual es probable que los vóxeles sean lesion
-  umbral = quantile(valores,0.15)+3*IQR(valores)
+  umbral = quantile(valores,0.75)+1.5*IQR(valores)
   #umbral = quantile(valores,0.999)
   #calculamos las coordenadas de esos vóxeles
   cordis = which(FLAIR>=umbral)
