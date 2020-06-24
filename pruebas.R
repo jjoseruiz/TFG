7f3cb3d5e4eacf0985d0162e252822348853462d
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-06-24 17:13:14 +0200

    pruebas2.0

--- pruebas.R
+++ pruebas.R
@@ -5,8 +5,6 @@ Si_T1_BRAIN = leeImagen(ROOT = "/Users/juanjoseruizpenela/Documents/IMG1/BRAIN_I
 BRAINS_T1=lapply(lB_T1,readnii)
 BRAINS_FLAIR=lapply(lB_FLAIR,readnii)
 ##Normalizado
-ind = whitestripe(img = S1_T1_BRAIN,type = "T1",stripped = TRUE)$whitestripe.ind
-ws_t1 = whitestripe_norm(S1_T1_BRAIN,indices = ind)
 
 ##
 t1_normalizada_zs = zscore_img(S1_T1_BRAIN,IMG_MASK)
@@ -66,19 +64,40 @@ library(ANTsRCore)
 library(extrantsr)
 library(oro.nifti)
 library(malf.templates)
-flair=antsImageRead("/Users/juanjoseruizpenela/Documents/IMG1/patient11-15/patient11/raw/patient11_FLAIR.nii.gz")
-t1=antsImageRead("/Users/juanjoseruizpenela/Documents/IMG1/patient11-15/patient11/raw/patient11_T1W.nii.gz")
-listaImagenes = preprocesadoPaciente(list(flair,t1))
-
 
 #PRUEBAS EN LA APP
-FLAIR = antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/S13_FLAIR_BRAIN.nii.gz")
-T1 = antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/S13_T1_BRAIN.nii.gz")
-FLAIR_SYM=antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S13_FLAIR_SIMETRICA.nii.gz")
-FLAIR_ASYM = antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S13_FLAIR_ASIMETRICA.nii.gz")
-T1_SYM = antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S13_T1_SIMETRICA.nii.gz")
-T1_ASYM = antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S13_T1_ASIMETRICA.nii.gz")
+s1_f = antsImageRead("s1_FLAIR_CORRECTED.nii.gz")
+
+FLAIR = antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/S2_FLAIR_BRAIN.nii.gz")
+FLAIR = readnii("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/S2_FLAIR_BRAIN.nii.gz")
+FLAIR = antsImageRead(paste0("/Users/juanjoseruizpenela/Documents/IMG1/raw_images/","patient",30,"_FLAIR.nii.gz"))
+ortho2(FLAIR_CORREGIDA3)
+T1 = antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/S2_T1_BRAIN.nii.gz")
+T1 = readnii("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/S2_T1_BRAIN.nii.gz")
+FLAIR_CORREGIDA3 = correccion(FLAIR_RAW)
+FLAIR_RAW = readnii("/Users/juanjoseruizpenela/Documents/IMG1/raw_images/patient30_FLAIR.nii.gz")
+FLAIR_RAW = antsImageRead("/Users/juanjoseruizpenela/Documents/IMG1/raw_images/patient1_FLAIR.nii.gz")
+T1_RAW = readnii("/Users/juanjoseruizpenela/Documents/IMG1/raw_images/patient30_T1W.nii.gz")
+brain_mask = readnii("/Users/juanjoseruizpenela/Documents/IMG1/raw_images/patient30_brainmask.nii.gz")
+brain_mask = antsImageRead("/Users/juanjoseruizpenela/Documents/IMG1/raw_images/patient1_brainmask.nii.gz")
+FLAIR_SYM=antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S2_FLAIR_SIMETRICA.nii.gz")
+FLAIR_SYM=readnii("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S2_FLAIR_SIMETRICA.nii.gz")
+FLAIR_ASYM = antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S2_FLAIR_ASIMETRICA.nii.gz")
+FLAIR_ASYM = readnii("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S2_FLAIR_ASIMETRICA.nii.gz")
+T1_SYM = antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S2_T1_SIMETRICA.nii.gz")
+T1_SYM = readnii("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S2_T1_SIMETRICA.nii.gz")
+T1_ASYM = antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S2_T1_ASIMETRICA.nii.gz")
+T1_ASYM = readnii("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S2_T1_ASIMETRICA.nii.gz")
+consenso= antsImageRead("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/CONSENSO/S2_CONSENSO.nii.gz")
+papayar::papaya(list(FLAIR,consenso))
 listaImagenes = list(FLAIR,T1,FLAIR_SYM,FLAIR_ASYM,T1_SYM,T1_ASYM)
+multi_overlay(listaImagenes)
+rm(FLAIR)
+rm(T1)
+rm(FLAIR_SYM)
+rm(FLAIR_ASYM)
+rm(T1_SYM)
+rm(T1_ASYM)
 source("obtenCoord.R")
 source("predice.R")
 source("preprocesadoPaciente.R")
@@ -89,6 +108,8 @@ source("sacoValorVecinos.R")
 source("aplicaFuncion.R")
 source("resultado.R")
 #proceso para predecir
+segme = antsImageRead("segmentacion_s2.nii.gz")
+
 cordis = eligeVoxelPaciente(listaImagenes[[1]])
 vecinos = recorreImagenes(listaImagenes,cordis)
 listaFunciones = c(mean,min,max,sd,median)
@@ -96,11 +117,74 @@ features = aplicaFuncion(vecinos,listaFunciones)
 lesiones=c(rep(1,nrow(features)))
 features = cbind2(features,lesiones)
 #modelo = readRDS("Knn_dataset2500.rds")
-modelo = readRDS("RandomForest5000.rds")
-modelo = readRDS("Bayesian5000.rds")
-prediccion = predice(modelo,features)
+modelo = readRDS("RandomForest5000_r2_v3.rds")
+modelo = readRDS("Bayesian5000_r2_v3.rds")
+modelo = readRDS("Knn5000_r2_v3.rds")
+prediccion_knn = predice(modelo,features)
 mask = resultado(listaImagenes[[1]],cordis,prediccion)
 #Representacion
 papayar::papaya(list(FLAIR,mask))
 #eliminamos todos los warnings que nos sale del bayesiano -1. Para activarlos 0.
 #options(warn = -1)
+
+
+
+#prueba comité
+#solo tiene sentido hacer la moda cuando hay más de 2 clasificadores.
+if(!is.null(predi_RF)&!is.null(prediKnn)&!is.null(prediBayes)){
+  print("hay randomforest, KNN y NB")
+  listaPred = list(predi_RF,prediKnn,prediBayes)
+  comite = c(1:length(cordis))
+  for(i in 1:length(comite)){
+    comite[i]=mfv(c(listaPred[[1]][[i]],listaPred[[2]][[i]],listaPred[[3]][[i]]))
+  }
+}
+
+
+
+#K-MEANS
+idx_foregorund<-FLAIR>0
+v<-FLAIR[idx_foregorund]
+cl<-kmeans(x=v,centers = 3)
+
+
+#
+centers<-cl$centers
+o<-order(centers)
+cluster<-cl$cluster
+cluster_new<-cluster
+for(i in 1:3){
+  cluster_new[cluster==o[i]]<-i
+}
+
+
+
+imagen_nueva<-FLAIR
+
+imagen_nueva[idx_foregorund]<-cluster_new
+imagen_nueva[imagen_nueva<0]<-0
+
+
+ortho2(imagen_nueva==2)
+
+ortho2(flair)
+
+ortho2(FLAIR)
+flair=correccion(FLAIR)
+library(fslr)
+ruta = "/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/S2_FLAIR_BRAIN.nii.gz"
+seg_nobias = fslr::fast_nobias(file = ruta)
+seg_fast=fast(file = FLAIR,type = "T2")
+corr_n4 = correccion(FLAIR)
+seg_nobias = fast_nobias()
+
+segme = antsImageRead("segmentacion_s2.nii.gz")
+
+red0.5=scales::alpha("red",0.5)
+
+##
+segmentacion = otropos(FLAIR,MASK)
+segme = antsImageRead("segmentacion_otropos.nii.gz")
+#otropos == 2 --> WhiteMatter
+writenii(segmentacion$segmentation,"segmentacion_otropos")
+###########
