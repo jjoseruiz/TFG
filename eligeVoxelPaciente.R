50bd454010c0e1b88cc5087be4983199300308b7
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-06-24 17:12:06 +0200

    prueba def eleccion voxeles paciente

--- eligeVoxelPaciente.R
+++ eligeVoxelPaciente.R
@@ -1,10 +1,22 @@
 eligeVoxelPaciente<-function(FLAIR){
   #Eliminamos los valores fondo
-  valores = FLAIR[FLAIR>0]
+  #seg = fslr::fast(FLAIR)
+  seg = segme
+  mascara_cereb = FLAIR>min(FLAIR)
+  #valores = FLAIR[seg == 2]
+  #im=which((seg==2)>0)
+  #valores = FLAIR[(seg==3)>0]
+  #im = FLAIR[im]
+  #mas=maskImage(FLAIR,im)
+  valores = FLAIR[mascara_cereb]
   #calculamos el umbral a partir del cual es probable que los vóxeles sean lesion
-  umbral = quantile(valores,0.75)+1.5*IQR(valores)
-  #umbral = quantile(valores,0.999)
+  umbral = quantile(valores,0.75)+0.75*IQR(valores)
   #calculamos las coordenadas de esos vóxeles
-  cordis = which(FLAIR>=umbral)
+  #cordis= which(FLAIR>=umbral)
+  cordis = which((seg == 2 | seg == 3) & (FLAIR>=umbral))
+
+  #wm=which(im==TRUE)
+  #cordis = which(is.element(wm,mayor_)==TRUE)
+  #cordis = mayor_[cordis]
   return(cordis)
 }
