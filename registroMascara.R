404bf1d98a98e8b60c694c7551d66957127ff938
jjoseruiz <61426095+jjoseruiz@users.noreply.github.com>
2020-06-25 19:16:41 +0200

    Delete registroMascara.R

--- registroMascara.R
+++
@@ -1,7 +0,0 @@
-registroMascara<-function(MASCARA)
-{
-  print("Ejecución Registro")
-  reg = extrantsr::registration(filename = MASCARA,template.file = S1_MASK,correct = FALSE, typeofTransform = "AffineFast",interpolator = "Linear",verbose = FALSE)
-  print("FIN DEL REGISTRO")
-  return (reg)
-}
