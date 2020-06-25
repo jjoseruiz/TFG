98036efee473319a679a1b0a133d878e049ab22f
jjoseruiz <61426095+jjoseruiz@users.noreply.github.com>
2020-06-25 19:17:09 +0200

    Delete registro.R

--- registro.R
+++
@@ -1,6 +0,0 @@
-registro<-function(IMG){
-  print("Ejecución Registro")
-  reg = extrantsr::registration(filename = IMG,template.file = s1_FLAIR_CORRECTED,correct = FALSE, typeofTransform = "AffineFast",interpolator = "Linear",verbose = FALSE)
-  print("FIN DEL REGISTRO")
-  return (reg)
-}
